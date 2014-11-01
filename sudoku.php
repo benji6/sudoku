22457b88ce9f7c53e3c5af39a280a700dda22c76
benji6 <benhall22@hotmail.co.uk>
2014-11-01 17:21:54 +0000

    update web site integration

--- sudoku.php
+++ sudoku.php
@@ -1,11 +1,16 @@
-<?php include 'includes/template.php'; ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php include '../includes/head_template.php' ?>
+	</head>
+	<body>
+		<?php include '../includes/header_template.php' ?>
 		<h2>Sudoku Solver</h2>
 		<button onFocus="if(this.blur)this.blur()" onClick="newPuzzle();">New Puzzle</button>
-		<br>
 		<button onFocus="if(this.blur)this.blur()" onClick="solver();">Solve!</button>	
 		<div id="pzl"></div>
 		<div id="msg" class="msg"></div>
 		<script src="sudoku.js"></script>
-		<script src="shifter.js"></script>
+		<?php include '../includes/template_scripts.php' ?>
 	</body>
 </html>
